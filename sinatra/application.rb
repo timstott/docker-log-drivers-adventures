2d9df1fa7bae10dc77567b11ff9bb9e0ee7db400
Timothy Stott <stott.timothy@gmail.com>
2015-08-18 18:09:02 +0100

    README and fluentd

--- sinatra/application.rb
+++ sinatra/application.rb
@@ -2,14 +2,24 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra/base'
 require 'logger'
+require 'json'
 
+STDOUT.sync = true
 
 class Application < Sinatra::Base
   set :bind, '0.0.0.0'
 
   get '/' do
-    logger = Logger.new(STDOUT.tap { |d| d.sync = true })
-    logger.info 'LOL'
+    STDOUT.puts JSON.dump("_service.name"=>"sinatra",
+                           "short_message"=>"Hello World in GELF",
+                           "timestamp"=>1439388274.077,
+                           "host"=>"TSTOTT.local",
+                           "level"=>7,
+                           "version"=>"1.1")
+
+    STDOUT.puts 'This is a long line' * 30_000
+
     'Hello World'
   end
 end
+
